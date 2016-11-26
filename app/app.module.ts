e9cca690ee439f30484f64e059b99d25e3c3f0d0
prems021 <premji.netcom@gmail.com>
2016-11-26 14:13:16 +0530

    Update app.module.ts

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,39 +1,25 @@
-import './rxjs-extensions';
-import { NgModule }       from '@angular/core';
-import {ReactiveFormsModule} from "@angular/forms";
-import { BrowserModule }  from '@angular/platform-browser';
-import { FormsModule }   from '@angular/forms';
-import { HttpModule }    from '@angular/http';
-import { AppComponent }         from './app.component';
-import { DashboardComponent }   from './dashboard/dashboard.component';
-import { MaterialModule } from '@angular/material';
-import { HeaderComponent } from './header/header.component';
-import { SidebarComponent } from './sidebar/sidebar.component';
-import { ContactComponent} from './contactus/contact.component';
-//import { TransportationComponent } from './dashboard/transaction/transaction.component';
+import { NgModule }      from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { HttpModule, JsonpModule } from '@angular/http';
 
-import { routing } from './routing';
-
-import { DataService } from './dashboard/data.service'
-
+import { AppComponent }  from './app.component';
+import { AboutComponent } from "./components/about/about.component";
+import { routing } from "./routes";
+import { HomeComponent } from "./components/home/home.component";
 
 @NgModule({
-  imports: [
-    BrowserModule,
-    ReactiveFormsModule,
-    FormsModule,
-    HttpModule, 
-    routing,MaterialModule.forRoot()
-  ],
-  declarations: [
-    AppComponent,HeaderComponent,SidebarComponent,ContactComponent,
-    DashboardComponent,
-   
-  ],
-  providers: [
-     DataService
-  ],
-  bootstrap: [ AppComponent ]
+    imports: [
+        BrowserModule,
+        HttpModule,
+        JsonpModule,
+        routing
+    ],
+    declarations: [
+        AppComponent,
+        AboutComponent,
+        HomeComponent
+    ],
+    bootstrap: [ AppComponent ]
 })
-export class AppModule {
-}
+export class AppModule { }
+
